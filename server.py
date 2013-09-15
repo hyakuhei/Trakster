7d424a0def5b9b59cb9e7def6e58ff7c068cacf4
Robert <hyakuhei@gmail.com>
2013-09-15 12:33:45 +0000

    Added Haversine algorithm for plotting distances from lat lon

--- server.py
+++ server.py
@@ -107,6 +107,25 @@ def kmlFile():
 	kml += kmlFooter
 	return kml
 	
+def haversine(pos1, pos2):
+    lat1 = float(pos1['lat'])
+    long1 = float(pos1['long'])
+    lat2 = float(pos2['lat'])
+    long2 = float(pos2['long'])
+
+    degree_to_rad = float(pi / 180.0)
+
+    d_lat = (lat2 - lat1) * degree_to_rad
+    d_long = (long2 - long1) * degree_to_rad
+
+    a = pow(sin(d_lat / 2), 2) + cos(lat1 * degree_to_rad) * cos(lat2 * degree_to_rad) * pow(sin(d_long / 2), 2)
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    km = 6367 * c
+    mi = 3956 * c
+
+    return {"km":km, "miles":mi}
+
+
 
 @app.route("/incoming", methods=['GET'])
 def incoming():
